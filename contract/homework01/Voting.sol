a44d58ba920c7dd7983cae7c69f001e8f57870bc
jarvis <weidongpo8875@163.com>
2026-06-26 20:53:59 +0800

    voting

--- contract/homework01/Voting.sol
+++ contract/homework01/Voting.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+/**
+ * 1. ✅ 创建一个名为Voting的合约，包含以下功能：
+ * 一个mapping来存储候选人的得票数
+ * 一个vote函数，允许用户投票给某个候选人
+ * 一个getVotes函数，返回某个候选人的得票数
+ * 一个resetVotes函数，重置所有候选人的得票数
+ */
+
 contract Voting {
     mapping(address => uint256) public ticketCount;
     address[] public candidates;
@@ -18,7 +26,6 @@ contract Voting {
 
     function vote(address user) public {
         require(user != address(0), "Invalid address");
-
         if (ticketCount[user] == 0) {
             candidates.push(user);
         }
